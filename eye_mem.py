f98c81a216648358136f7cfc34ae18d04cc2e966
Blake Blount <bblount@sefcor.com>
2024-08-21 16:29:57 -0400

    Update eye

--- eye_mem.py
+++ eye_mem.py
@@ -3,13 +3,13 @@ import numpy as np
 import cv2
 
 cam0 = cv2.VideoCapture(0)
-cam1 = cv2.VideoCapture(1)
+#cam1 = cv2.VideoCapture(1)
 #cam2 = cv2.VideoCapture(2)
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out0 = cv2.VideoWriter('output.avi', fourcc, 40.0, (640, 480))
-out1 = cv2.VideoWriter('output.avi', fourcc, 40.0, (640, 480))
+#out1 = cv2.VideoWriter('output.avi', fourcc, 40.0, (640, 480))
 #out2 = cv2.VideoWriter('output.avi', fourcc, 40.0, (640, 480))
 
 
@@ -17,7 +17,7 @@ out1 = cv2.VideoWriter('output.avi', fourcc, 40.0, (640, 480))
 while(True):
     # reads frames from a camera 
     # ret checks return at each frame
-    ret, frame = cam0.read()
+    ret, frame = cap.read()
 
     # output the frame
     out.write(frame)
